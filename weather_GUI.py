c8ed9f06f1c6acc6666a9e54eccd8b80a499a003
Alex Zafiris <zafiris.alex@yahoo.com>
2020-02-12 12:23:27 +0200

    Update weather_GUI.py

--- weather_GUI.py
+++ weather_GUI.py
@@ -62,7 +62,7 @@ def get_forecast():
     label1.config(font=('verdana', 9, 'bold'))
     canvas1.create_window(320, 240, window=label1)
     
-    if data is not NULL:
+    if data is not None:
         label2 = tk.Label(root, text='JSON file created')
         label2.config(font=('verdana', 9, 'bold'))
         canvas1.create_window(320, 260, window=label2)
